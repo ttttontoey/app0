052617151157b39ae834465217d54fd2ef242cb7
ttt.tontoey <0928934814b@gmail.com>
2025-07-14 11:42:27 +0700

    css external
    css external

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import react from 'react'
+import'./style.css'
 
 function App() {
-  const divStyle ={
-    color:'red',
-    backgroundColor:'powderblue',
-    fontSize:'larger',
-    padding:'3px'
-  }
-  return <div style={divStyle}>Hello React</div>
+  return(
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  ) 
 }
 
 export default App;
