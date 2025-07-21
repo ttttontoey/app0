a353f470dcd76743f35cbe50e57832a0ca85ee75
ttt.tontoey <0928934814b@gmail.com>
2025-07-21 11:00:12 +0700

    Function Components
    Function Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import react from 'react'
-import'./style.css'
+// import'./style.css'
+import { Content, Header, Footer } from './func-components';
 
 function App() {
-  return(
-    <>
-    <h2>DeveloperThai.com</h2>
-    <div className='title'>React & React Native</div>
-    </>
-  ) 
+  return([<Header/>,<Content/>,<Footer/>])
+  // return(
+  //   <>
+  //   <h2>DeveloperThai.com</h2>
+  //   <div className='title'>React & React Native</div>
+  //   </>
+  // ) 
 }
 
 export default App;
