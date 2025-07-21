cc9f4ce6eb1d88039d8795f858efbf49a8b2d3a8
ttt.tontoey <0928934814b@gmail.com>
2025-07-21 11:41:34 +0700

    Date
    date

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import react from 'react'
 // import'./style.css'
 import { Content, Header, Footer } from './func-components';
+import Calendar from './class-components';
 
 function App() {
-  return([<Header/>,<Content/>,<Footer/>])
+  return(
+  <>
+  <Header/>
+  <p><center> <Calendar/> </center></p>
+  <Content/>
+  <Footer/>
+  </>
+  )
   // return(
   //   <>
   //   <h2>DeveloperThai.com</h2>
