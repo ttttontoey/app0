c6b12fba50290cded2fe228d2b6c2b90b2a44563
ttt.tontoey <0928934814b@gmail.com>
2025-08-04 09:53:08 +0700

    error in class components
    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import react from 'react'
 // import'./style.css'
 import { Content, Header, Footer } from './func-components';
-import Calendar from './class-components';
+// import Calendar from './class-components';
+import Button from './class-components';
 
 function App() {
-  return(
-  <>
-  <Header/>
-  <p><center> <Calendar/> </center></p>
-  <Content/>
-  <Footer/>
-  </>
-  )
+  return <Button/>
+}
+
+export default App;
+
+// return(
+  // <>
+  // <Header/>
+  // <p><center> <Calendar/> </center></p>
+  // <Content/>
+  // <Footer/>
+  // </>
+  // )
   // return(
   //   <>
   //   <h2>DeveloperThai.com</h2>
   //   <div className='title'>React & React Native</div>
   //   </>
   // ) 
-}
-
-export default App;
