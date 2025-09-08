c8fdbd340993890a2fb5dcb1119c09502594247a
ttt.tontoey <0928934814b@gmail.com>
2025-09-08 10:04:52 +0700

    add navlink

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import { useState } from "react";
-import Menu from "./component/Menu";
-import Quiz from "./component/Quiz";
-import Score from "./component/score";
-import { createContext } from "react";
+import React from 'react'
+import Router2 from "./router2"
 
-import './App.css'
-
-export const DataContext = createContext()
-
-function App() {
-  const [appState,setAppState] = useState("menu")
-  const [score,setScore] = useState(0) 
-  return(
-    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
-      <div className="App">
-        <h1>Web Development Quiz</h1>
-        {appState === "menu" && <Menu/>}
-        {appState === "quiz" && <Quiz/>}
-        {appState === "score" && <Score/>}
-      </div>
-    </DataContext.Provider>
-  );
+export default function App() {
+return <Router2/>
 }
 
-export default App;
+
+// import { useState } from "react";
+// import Menu from "./component/Menu";
+// import Quiz from "./component/Quiz";
+// import Score from "./component/score";
+// import { createContext } from "react";
+
+// import './App.css'
+
+// export const DataContext = createContext()
+
+// function App() {
+//   const [appState,setAppState] = useState("menu")
+//   const [score,setScore] = useState(0) 
+//   return(
+//     <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+//       <div className="App">
+//         <h1>Web Development Quiz</h1>
+//         {appState === "menu" && <Menu/>}
+//         {appState === "quiz" && <Quiz/>}
+//         {appState === "score" && <Score/>}
+//       </div>
+//     </DataContext.Provider>
+//   );
+// }
+
+// export default App;
 
 
 
