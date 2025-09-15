f33b6e85753c7799631c581d268cd3f3c30b106e
ttt.tontoey <0928934814b@gmail.com>
2025-09-15 10:58:06 +0700

    FormSelectFile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import FormText from './form-text';
+// import FormText from './form-text';
+import FormSelectFile from './form-select-file';
 export default function App() {
-  return <FormText />;
+  return <FormSelectFile />;
 }
 
 
